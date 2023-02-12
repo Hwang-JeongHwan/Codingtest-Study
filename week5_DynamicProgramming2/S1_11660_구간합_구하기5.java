9c44c1b786bb855e02db328f85fe72d416337d14
Hwang-JeongHwan <jeonghwan97@naver.com>
2023-02-12 19:32:22 +0900

    week5

--- week5_DynamicProgramming2/S1_11660_구간합_구하기5.java
+++ week5_DynamicProgramming2/S1_11660_구간합_구하기5.java
@@ -8,28 +8,32 @@ public class S1_11660_구간합_구하기5 {
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n+1][n+1];
-        int[][] dp = new int[n+1][n+1];
-        for (int i = 1; i <= n; i++) {
+
+        int [][] arr = new int[n + 1][n + 1];
+        int [][] dp = new int[n + 1][n + 1];
+        for(int i = 1; i < n + 1; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++) {
+            for(int j = 1; j < n + 1; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j];
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < n + 1; j ++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j]; // 중복으로 더해지는 부분 빼고 저장
             }
         }
-        for (int k = 1; k <= m; k++) {
+//        System.out.println(Arrays.deepToString(arr));
+//        System.out.println(Arrays.deepToString(dp));
+        for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            int ans = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
-            sb.append(ans + "\n");
+//            System.out.println(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]);
+            sb.append(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] + "\n");
+
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
--- week5_DynamicProgramming2/S1_21317_징검다리_건너기.java
+++ week5_DynamicProgramming2/S1_21317_징검다리_건너기.java
@@ -3,45 +3,37 @@ import java.util.*;
 import java.io.*;
 
 public class S1_21317_징검다리_건너기 {
-    static int n;
-    static int answer = Integer.MAX_VALUE;
-    static int [][] arr;
-    static int k;
+    static final int max_num = 20001;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        arr = new int[n+1][2];
-        int small;
-        int big;
-        for(int i=1; i<=n-1; i++) {
-            String[] s = br.readLine().split(" ");
-            small = Integer.parseInt(s[0]);
-            big = Integer.parseInt(s[1]);
-            arr[i][0]=small;
-            arr[i][1]=big;
+
+        int[][] arr = new int[20][2];
+        int[][] dp = new int[21][2];
+        for(int i = 1; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+
         }
-        k = Integer.parseInt(br.readLine());
-
-        dfs(0,1,false);
-        System.out.println(answer);
-    }
-
-    private static void dfs(int sum, int to, boolean flag){
-        if(to==n){
-            answer = Math.min(answer,sum);
-            return;
+        for(int i = 0; i < n + 1; i ++){
+            dp[i][0] = max_num;
+            dp[i][1] = max_num;
         }
-
-        if(to>n){
-            return;
+        int k = Integer.parseInt(br.readLine());
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 0;
+        dp[2][0] = arr[1][0];
+        dp[3][0] = Math.min(arr[1][1], dp[2][0] + arr[2][0]);
+        for(int i = 4; i < n + 1; i++){
+            dp[i][0] = Math.min(dp[i - 1][0] + arr[i - 1][0], dp[i - 2][0] + arr[i - 2][1]);
+            dp[i][1] = Math.min(Math.min(dp[i - 1][1] + arr[i - 1][0], dp[i - 2][1] + arr[i - 2][1]), dp[i - 3][0] + k);
         }
-
-        dfs(sum+arr[to][0],to+1,flag); // 한 칸 넘어갈 때
-        dfs(sum+arr[to][1],to+2,flag); // 두 칸 넘어갈 때
-
-        if(!flag){ // 한 번 주어지는 세 칸 넘어갈 때 체크해줍니다.
-            dfs(sum+k, to+3, true);
-        }
+//        System.out.println(Arrays.deepToString(dp));
+        System.out.println(Math.min(dp[n][0], dp[n][1]));
     }
 }
