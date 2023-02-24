99b20c50cff2d100ab39b3e673b69659c4afae7b
Hwang-JeongHwan <jeonghwan97@naver.com>
2023-02-24 13:55:22 +0900

    week7

--- week7/S5_1789_수들의_합.java
+++ week7/S5_1789_수들의_합.java
@@ -1,19 +1,24 @@
 package week7;
 import java.util.*;
-
+import java.io.*;
 public class S5_1789_수들의_합 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long s = Long.parseLong(br.readLine());
+        int i = 0;
+        long sum = 0;
 
-        long n = sc.nextLong();
-        long sum = 0, cnt = 0;
-        for (int i = 1; i <= n; i++) {
-            if(sum > n) break;
+        while(sum < s) {
+            i++;
             sum += i;
-            cnt++;
+        }
+        if(sum > s) {
+            System.out.println(i-1);
+        }else {
+            System.out.println(i);
         }
 
-        System.out.println(cnt-1);
-    }
-}
 
+    }
+
+}
