04db3e9ffe217f7c9d2c52b7639c76dee9295896
Hwang-JeongHwan <jeonghwan97@naver.com>
2023-03-11 02:31:37 +0900

    week9

--- week9_TwoPointer/G4_13144_List_of_Unique_Numbers.java
+++ week9_TwoPointer/G4_13144_List_of_Unique_Numbers.java
@@ -13,7 +13,7 @@ public class G4_13144_List_of_Unique_Numbers {
     static void input(){
         n = scan.nextInt();
         arr = new int[n + 1];
-        count = new int[n + 1];
+        count = new int[100000 + 1];
         for(int i = 1; i <= n ; i++){
             arr[i] = scan.nextInt();
         }
@@ -22,11 +22,16 @@ public class G4_13144_List_of_Unique_Numbers {
         int right = 0;
         long total = 0;
         for(int left = 1; left <= n; left++){
+            //right를 옮길 수 있는 만큼 옮긴다
+            // 다음 count배열의 arr의 다음 위치가 0 이면 옮길 수 있음
             while(right + 1 <= n && count[arr[right + 1]] == 0){
                 right++;
                 count[arr[right]]++;
             }
+            // total을 갱신
             total += right - left + 1;
+
+            //left를 옮겨주면서 count[arr[left]]를 1감소시킨다.
             count[arr[left]]--;
         }
         System.out.println(total);
